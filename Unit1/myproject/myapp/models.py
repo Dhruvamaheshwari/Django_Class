5c7b5c54e89d6c0720e79e46aec12c7c700799c7
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-08-21 15:31:29 +0530

    feat: add views and URL routing for myapp

--- Unit1/myproject/myapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class employee(models.model):
-    name=models.CharField(max_length=100)
-    age=models.IntegerField()
--- Unit1/myproject/myapp/views.py
+++ Unit1/myproject/myapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def hello(request):
+    name = "Dhruva Maheshwari"
+    return HttpResponse(f"Hello my name is {name}")
+
+def home(req):
+    return HttpResponse('<h1 style="color:blue" >Welcome to the Home page</h1>')
+
+def about(req):
+    return HttpResponse('This is the about page')
+
+
+def result(req):
+    mark = 60
+    if(mark < 50):
+        return HttpResponse("not Good")
+    elif(mark >= 50 and mark <=70):
+        return HttpResponse("good")
+    elif(mark >= 70 and mark <=90):
+        return HttpResponse("Excellent")
+    else:
+        return HttpResponse("very Excellent")
+    
